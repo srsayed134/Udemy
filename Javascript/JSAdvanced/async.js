7f604237f78758f0b24a94e62dc5fd94970cc105
srsayed134 <srsayed134@gmail.com>
2025-06-28 14:41:04 +0600

    Learned promises

--- Javascript/JSAdvanced/async.js
+++ Javascript/JSAdvanced/async.js
@@ -1,82 +1,116 @@
-//console.log("One");
-//console.log("Two");
+// //console.log("One");
+// //console.log("Two");
 
-setTimeout(()=>{
-    //console.log("Hello");  
-}, 1000);
+// const { reject } = require("async");
+
+// setTimeout(()=>{
+//     //console.log("Hello");  
+// }, 1000);
  
-//console.log("Three");
-//console.log("Four");
+// //console.log("Three");
+// //console.log("Four");
 
-//Callback function
+// //Callback function
 
 
-/*function sum (a, b){
-  console.log(a + b);
-}*/
+// /*function sum (a, b){
+//   console.log(a + b);
+// }*/
 
-function calculate(a, b, sumCallBack){
-  sumCallBack(a, b);
-}
+// function calculate(a, b, sumCallBack){
+//   sumCallBack(a, b);
+// }
 
-calculate(1, 2, (a, b) => {
-  //console.log(a + b);
+// calculate(1, 2, (a, b) => {
+//   //console.log(a + b);
   
-})
-// Settimeout with callback
-const hello = () => {
-  //console.log("This is callback")
-}
+// })
+// // Settimeout with callback
+// const hello = () => {
+//   //console.log("This is callback")
+// }
 
-setTimeout(hello, 2000)
-// Newsting for loop
-for(let i = 0; i < 5; i++){
-  let str = "";
-  for(let j = 0; j < 5; j++){
-    str = str + j;
-  }
-  //console.log(i, str)
-}
+// setTimeout(hello, 2000)
+// // Newsting for loop
+// for(let i = 0; i < 5; i++){
+//   let str = "";
+//   for(let j = 0; j < 5; j++){
+//     str = str + j;
+//   }
+//   //console.log(i, str)
+// }
  
-//getting data for delay exercise with callback hell
+// //getting data for delay exercise with callback hell
 
-/* function getData(dataID ,getNextData){
-  setTimeout(()=>{
-    console.log("Data :", dataID);
-    if(getNextData){
-      getNextData();
-    }
-  }, 3000)
-}
+// /* function getData(dataID ,getNextData){
+//   setTimeout(()=>{
+//     console.log("Data :", dataID);
+//     if(getNextData){
+//       getNextData();
+//     }
+//   }, 3000)
+// }
 
-getData(1, ()=>{
-  getData(2, () =>{
-    getData(3, () => {
-      getData(4)
-    });
-  })
-})
-*/
+// getData(1, ()=>{
+//   getData(2, () =>{
+//     getData(3, () => {
+//       getData(4)
+//     });
+//   })
+// })
+// */
 
-/*
-function getData(myData, nextData){
-    setTimeout(()=>{
-      console.log("Data :", myData);
-      if(nextData){
-        nextData();
-      }
-    }, 500)
-}
+// /*
+// function getData(myData, nextData){
+//     setTimeout(()=>{
+//       console.log("Data :", myData);
+//       if(nextData){
+//         nextData();
+//       }
+//     }, 500)
+// }
 
-getData(1, () => {
-  getData(2, () => {
-    getData(3)
-  })
-}) */
+// getData(1, () => {
+//   getData(2, () => {
+//     getData(3)
+//   })
+// }) */
 
-//Promises
+// //Promises
 
-let promise = new Promise((resolve, reject)=>{
+// // let promise = new Promise((resolve, reject)=>{
+// //     console.log("I am a promise");
+// //     reject("Some error occurd")
+// // })
+
+
+// function getData(dataId, getNextData){
+//   return new Promise((resolve, reject) => {
+//     setTimeout(()=>{
+//       console.log("data", dataId);
+//       resolve("Success")
+//       if (getNextData) {
+//         getNextData();
+//       }
+//     }, 7000)
+//   })
+// } 
+
+const getPromise = () => {
+  return new Promise ((resolve, reject) =>{
     console.log("I am a promise");
-    reject("Some error occurd")
+    resolve("Success");
+    // reject("Rejected")
+});
+};
+
+let holdPromise = getPromise();
+
+holdPromise.then((result) => {
+  console.log("Promiss fullfiled", result);
+});
+
+holdPromise.catch((error)=>{
+  console.log("Promise rejected", error);
+  
 })
